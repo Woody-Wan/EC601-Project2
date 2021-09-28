9e0db29480c1ae469c4632fd5136e743024b726d
Woody-Wan <63642698+Woody-Wan@users.noreply.github.com>
2021-09-29 04:27:41 +0800

    Add files via upload

---
+++ GoogleAPI.py
@@ -0,0 +1,32 @@
+from google.cloud import language_v1
+
+def sample_analyze_sentiment(text_content):
+    """
+    Analyzing Sentiment in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+    print(
+        u"Document sentiment magnitude: {}".format(
+            response.document_sentiment.magnitude
+        )
+    )
+    for sentence in response.sentences:
+        print(u"Sentence text: {}".format(sentence.text.content))
+        print(u"Sentence sentiment score: {}".format(sentence.sentiment.score))
+        print(u"Sentence sentiment magnitude: {}".format(sentence.sentiment.magnitude))
+
+    print(u"Language of the text: {}".format(response.language))
